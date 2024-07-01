ae39e58a8a7fec5157e9a6efec997da76f33eb0e
joseph <langj6757@gmail.com>
2024-07-01 13:04:25 -0700

    Edit appointment button style

--- src/components/appointment-btn/appointment-btn.jsx
+++ src/components/appointment-btn/appointment-btn.jsx
@@ -1,7 +1,11 @@
 const AppointmentButton = () => {
   return (
-    <button className="bg-[#663130] text-white h-min px-10 py-2 hover:bg-transparent hover:text-[#191717] border border-[#663130] transition-colors duration-300 tracking-[3px] text-sm uppercase font-[poppins]">
-      book appointment
+    <button className="relative inline-block px-4 py-2 font-medium group">
+      <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1.5 translate-y-1.5 bg-[#663130] group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+      <span className="absolute inset-0 w-full h-full bg-white border-2 border-[#663130] group-hover:bg-[#663130]"></span>
+      <span className="relative text-[#663130] font-[poppins] group-hover:text-white">
+        Book Appointment
+      </span>
     </button>
   );
 };
